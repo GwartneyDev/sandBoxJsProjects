b7535899d75dcd22cbf7a3ab41c3e2edb39d2e00
GwartneyDev <gwartneydev@gmail.com>
2023-12-25 16:38:17 -0800

    Added in reading from sector ID to get entery DiID
    Reading the Directory stream entry given the sector ID values

--- xlsExportTest.js
+++ xlsExportTest.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const filePath = 'test4.xls';
+
 const dynamicBufferSize = 512; // Replace with the actual size you want to read
 const dynamicStartOffset = 0x40; // Replace with the actual offset you want to start from
 
-readDynamicData(filePath, dynamicBufferSize, dynamicStartOffset);
 function readDynamicData(filePath, bufferSize, startOffset) {
   const buffer = Buffer.alloc(bufferSize);
 
@@ -26,41 +26,45 @@ function readDynamicData(filePath, bufferSize, startOffset) {
           return;
         }
 
-        // MSAT SECTOR INFORMATION
-        const msatSector = Buffer.from(buffer);
+        // const msatDirectoryTest = Buffer.alloc(512);
+        // fs.readSync(fd, msatDirectoryTest, 0, 512, 0x40); // Assuming the offset is 8 bytes after the
 
-        // Extract the SecID of the first sector of the master sector allocation table
-        const firstSectorID = msatSector.readInt32LE(68);
+        // const readData = msatDirectoryTest.readInt32LE(8);
+        // console.log(readData);
 
-        // Extract the number of sectors used by the MSAT
-        const sectorsUsedByMSAT = msatSector.readInt32LE(4);
+        const Directory = Buffer.alloc(512);
+        fs.readSync(fd, Directory, 0, 512, 0x30); // Assuming the offset is 8 bytes after the
+        const readDirectory = Directory.readInt32LE(0);
+        console.log(readDirectory);
 
-        console.log('SecID of the first sector:', firstSectorID);
-        console.log('Number of sectors used by MSAT:', sectorsUsedByMSAT);
+        // // Read the next 436 bytes containing the first 109 SecIDs of the MSAT
+        // const msatEntriesBuffer = Buffer.alloc(436);
+        // fs.readSync(fd, msatEntriesBuffer, 0, 436, startOffset + 8); // Assuming the offset is 8 bytes after the start
 
-        // Read the next 436 bytes containing the first 109 SecIDs of the MSAT
-        const msatEntriesBuffer = Buffer.alloc(436);
-        fs.readSync(fd, msatEntriesBuffer, 0, 436, startOffset + 8); // Assuming the offset is 8 bytes after the start
+        // // Parse the SecIDs
+        // const secIds = [];
+        // for (let i = 0; i < msatEntriesBuffer.length; i += 4) {
+        //   const secId = msatEntriesBuffer.readInt32LE(i);
+        //   secIds.push(i === 0 ? secId : -1); // Set all remaining SecIDs to -1 except the first one
+        // }
 
-        // Parse the SecIDs
-        const secIds = [];
-        for (let i = 0; i < msatEntriesBuffer.length; i += 4) {
-          const secId = msatEntriesBuffer.readInt32LE(i);
-          secIds.push(i === 0 ? secId : -1); // Set all remaining SecIDs to -1 except the first one
-        }
-
-        console.log('First 109 SecIDs of the MSAT:', secIds);
+        // console.log('First 109 SecIDs of the MSAT:', secIds);
 
         ///READ THE VALUES IN SEC 0;
-        const msatEntriesBuffer2 = Buffer.alloc(512);
-        fs.readSync(fd, msatEntriesBuffer2, 0, 512, 0x200); // Assuming the offset
-        const neSecIid = [];
-        for (let i = 0; i < msatEntriesBuffer2.length; i += 4) {
-          const secId2 = msatEntriesBuffer2.readInt32LE(i);
-          neSecIid.push(secId2);
-        }
 
-        console.log(neSecIid);
+        // const msatEntriesBuffer2 = Buffer.alloc(512);
+
+        // fs.readSync(fd, msatEntriesBuffer2, 0, 512, 0x200); // Assuming the offset
+
+        // const neSecIid = [];
+
+        // for (let i = 0; i < msatEntriesBuffer2.length; i += 4) {
+        //   const secId2 = msatEntriesBuffer2.readInt32LE(i);
+
+        //   neSecIid.push(secId2);
+        // }
+
+        // console.log(neSecIid);
 
         fs.close(fd, (err) => {
           if (err) {
